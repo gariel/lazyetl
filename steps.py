c7876ab82176fd18b1a27e9bb0af63529bdb2247
Gabriel Sant'Ana Pereira <gariel@ymail.com>
2019-01-06 18:31:02 -0200

    better step resolution and saving steps metadata with decorator

--- steps.py
+++ steps.py
@@ -1,27 +1,45 @@
 
-class ExecutionStep:
-    def execute(self):
-        pass
+execution_steps_definition = {}
 
-class PrintExecutionStep(ExecutionStep):
-    # in text str
+
+def executionStep(key, input={}, output={}):
+    def decorator(clzz):
+        fields = input.copy()
+        fields.update(output)
+        execution_steps_definition[key] = clzz
+
+        clzz.__step_input__ = input
+        clzz.__step_output__ = output
+        clzz.__step_key__ = key
+
+        init = clzz.__init__
+        def esInit(self, *args, **kwargs):
+            for field, valuefunc in fields.items():
+                setattr(self, field, valuefunc())
+            init(self, *args, **kwargs)
+        clzz.__init__ = esInit
+        return clzz
+    return decorator
+
+
+@executionStep("Print",
+    input={"text": str})
+class PrintExecutionStep:
     def execute(self):
         print(self.text)
 
-class UpperCaseExecutionStep(ExecutionStep):
-    # in text str
-    # out result str
+
+@executionStep("UpperCase",
+    input={"text": str},
+    output={"result": str})
+class UpperCaseExecutionStep:
     def execute(self):
         self.result = self.text.upper()
 
-class UserInputExecutionStep(ExecutionStep):
-    # in message str
-    # out input str
+
+@executionStep("UserInput",
+    input={"text": str},
+    output={"input": str})
+class UserInputExecutionStep:
     def execute(self):
         self.input = input(self.message)
-
-execution_steps_definition = {
-    "Print": PrintExecutionStep,
-    "UpperCase": UpperCaseExecutionStep,
-    "UserInput": UserInputExecutionStep
-}
